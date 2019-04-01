f79da354f6e8f14e43cb3e5e3bb3c10c13d6ca34
Sean Greenhow <sean.greenhow@honestas-finanzmanagement.de>
2019-04-01 17:11:29 +0200

    stuff

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,10 +1,11 @@
 import * as React from "react"
 import Router from "./pages/Router";
+import Gate from "./components/Gate";
 
 export default class Root extends React.Component {
     render() {
         return <div className="fill">
-
+            <Gate />
             <Router />
         </div>
     }
--- src/components/Gate/index.tsx
+++ src/components/Gate/index.tsx
@@ -4,9 +4,15 @@ import './Gate.scss'
 export default class Gate extends React.Component {
     render() {
         return (
-            <div className="gate">
+            <div className="gate fill">
+
                 <div className="door right"></div>
                 <div className="door left"></div>
+
+                <div className="lock">
+                    <div className="seal right"></div>
+                    <div className="seal left"></div>
+                </div>
             </div>
         )
     }
