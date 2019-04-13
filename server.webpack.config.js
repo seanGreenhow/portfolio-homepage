d66e93ba01da39016cd3701dfc2c711badfb17f8
Sean Greenhow <sean@hamburg.de>
2019-04-13 19:53:12 +0200

    fixed server production build

--- server.webpack.config.js
+++ server.webpack.config.js
@@ -1,52 +1,54 @@
-const path = require( 'path' );
-const webpack = require( "webpack" )
-const nodeExternals = require( 'webpack-node-externals' );
+const path = require('path');
+const webpack = require("webpack")
+const nodeExternals = require('webpack-node-externals');
 
-module.exports = ( env, argv ) => {
-  if ( argv.mode == "production" ) plugins.push( new webpack.DefinePlugin( {
-    'process.env.NODE_ENV': JSON.stringify( 'production' )
-  } ) )
+module.exports = (env, argv) => {
+  let plugins = []
+  if (argv.mode == "production") plugins.push(new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify('production')
+  }))
 
-  return ( {
+  return ({
     entry: [
       './srv'
     ],
     output: {
-      path: path.resolve( __dirname, 'build' ),
+      path: path.resolve(__dirname, 'build'),
       filename: 'server.js',
       library: 'app',
       libraryTarget: 'commonjs2'
     },
     target: 'node',
-    externals: [ nodeExternals() ],
+    externals: [nodeExternals()],
     watchOptions: {
       aggregateTimeout: 500,
-      ignored: [ /build/, /node_modules/ ]
+      ignored: [/build/, /node_modules/]
     },
+    plugins,
     module: {
-      rules: [ {
-          test: /\.jsx?$/,
-          use: [
-            'babel-loader'
-          ],
-          exclude: '/node_modules/'
-        },
-        {
-          test: /\.tsx?$/,
-          use: [
-            'babel-loader',
-            'ts-loader'
-          ],
-          exclude: '/node_modules/'
-        },
-        {
-          test: /\.(s?css|ttf|otf|eot|woff2?|html|ico|png|jpe?g)$/,
-          use: [ "null-loader" ]
-        }
+      rules: [{
+        test: /\.jsx?$/,
+        use: [
+          'babel-loader'
+        ],
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.tsx?$/,
+        use: [
+          'babel-loader',
+          'ts-loader'
+        ],
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.(s?css|ttf|otf|eot|woff2?|html|ico|png|jpe?g)$/,
+        use: ["null-loader"]
+      }
       ]
     },
     resolve: {
-      extensions: [ '.ts', '.js', '.tsx', '.jsx', '.json' ]
+      extensions: ['.ts', '.js', '.tsx', '.jsx', '.json']
     }
-  } )
+  })
 }
